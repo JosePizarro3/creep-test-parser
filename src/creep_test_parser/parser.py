04ce98572028641aaf312109cb3c0e82a2895bae
jpizarro <jose.pizarro-blanco@bam.de>
2026-01-22 16:52:07 +0100

    Add basic custom excel parsing
    Add env.example
    
    Add tutorials with script

--- src/creep_test_parser/parser.py
+++ src/creep_test_parser/parser.py
@@ -1,14 +1,51 @@
-from bam_masterdata.datamodel.object_types import ExperimentalStep
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+# from bam_masterdata.datamodel.object_types import ExperimentalStep
 from bam_masterdata.parsing import AbstractParser
+
+
+class BaseFileParser(ABC):
+    @abstractmethod
+    def custom_parser(self):
+        pass
+
+
+class ExcelParser(BaseFileParser):
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+
+    def custom_parser(self):
+        df_source = pd.read_excel(
+            self.filepath, sheet_name=None, header=0, engine="openpyxl", dtype=str
+        )
+        # row index where the real header lives
+        header_row = 6
+
+        new_df = {}
+        for key, df in df_source.items():
+            # extract header
+            new_header = df.iloc[header_row]
+
+            # slice data below header
+            df = df.iloc[header_row + 1 :].copy()
+
+            # assign header
+            df.columns = new_header
+            # cleanup
+            new_df[key] = df.reset_index(drop=True)
+
+        return new_df
 
 
 class CreepTestParser(AbstractParser):
     def parse(self, files, collection, logger):
-        synthesis = ExperimentalStep(name="Synthesis")
-        synthesis_id = collection.add(synthesis)
-        measurement = ExperimentalStep(name="Measurement")
-        measurement_id = collection.add(measurement)
-        _ = collection.add_relationship(synthesis_id, measurement_id)
-        logger.info(
-            "Parsing finished: Added examples synthesis and measurement experimental steps."
-        )
+        for file in files:
+            if not file.endswith(".xlsx"):
+                logger.error(f"CreepTestParser: Unsupported file type {file}")
+                continue
+
+            df = ExcelParser(file).custom_parser()
+            # map into openBIS object types
+            # ...
---
+++ tutorials/parser_tutorial.py
@@ -0,0 +1,32 @@
+from decouple import config as environ
+from pybis import Openbis
+
+# Connect to openBIS
+# Add a file in your home directory named `.env` with the following content:
+# OPENBIS_URL=your_openbis_url
+# OPENBIS_USERNAME=your_username
+# OPENBIS_PASSWORD=your_password
+openbis = Openbis(environ("OPENBIS_URL"))
+openbis.login(environ("OPENBIS_USERNAME"), environ("OPENBIS_PASSWORD"), save_token=True)
+
+
+from bam_masterdata.cli.run_parser import run_parser
+
+from creep_test_parser.parser import CreepTestParser
+
+# Define which parser to use and which files to parse
+files_parser = {
+    CreepTestParser(): [
+        "./tests/data/data_2025-06_Data-Schema_Creep_v1.1_EN AW 2618_v1.0.xlsx",
+    ]
+}
+
+# Run the parser
+run_parser(
+    openbis=openbis,
+    space_name="VP.1_JPIZARRO",
+    project_name="CREEPTEST_PROJECT",
+    collection_name="CREEPTEST_TEST_COLLECTION",
+    files_parser=files_parser,
+)
+print("Parsing completed.")
